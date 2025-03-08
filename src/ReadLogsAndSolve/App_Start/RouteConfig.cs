cc0a7246f80cd3a7a7cb0bcce31ffe205d4b0716
Ashutosh Jha <108254506+ashu7228@users.noreply.github.com>
2025-03-08 21:35:34 +0530

    added the Solution file for Visual Studio

---
+++ src/ReadLogsAndSolve/App_Start/RouteConfig.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ReadLogsAndSolve
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                 name: "AnalyizLogs",
+                 url: "api/{controller}/{action}",
+                 defaults: new { controller = "{controller}", action = "{action}" }
+             );
+        }
+    }
+}
